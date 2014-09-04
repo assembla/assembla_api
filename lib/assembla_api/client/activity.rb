52f35251a290fc7fd4bfc98ef6104611e5bfd4a3
Vitalie Lazu <vitalie@assembla.com>
2014-09-04 20:35:03 +0300

    Added params validation for /v1/activity

--- lib/assembla_api/client/activity.rb
+++ lib/assembla_api/client/activity.rb
@@ -2,9 +2,24 @@
 
 module Assembla
   class Client::Activity < API
+    VALID_REQUEST_PARAM_NAMES = %w[
+      space_id
+      to
+      from
+    ].freeze
+
+    TIME_REGEXP = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/
+
+    VALID_REQUEST_PARAM_VALUES = {
+      'to' => TIME_REGEXP,
+      'from' => TIME_REGEXP
+    }
 
     def list(*args)
-      arguments(args)
+      arguments(args) do
+        permit VALID_REQUEST_PARAM_NAMES
+        assert_values VALID_REQUEST_PARAM_VALUES
+      end
 
       response = get_request("/activity", arguments.params)
       return response unless block_given?
